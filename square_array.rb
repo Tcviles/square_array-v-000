f0a6e016063652903440c888be632d0b5e20642a
Thomas Viles <thomas.c.viles.jr@gmail.com>
2019-06-09 20:03:30 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,3 @@
 def square_array(array)
-  new_array = []
-  array.each do |number|
-    new_number = number * number
-    new_array.push (new_number)
-  end
-  return new_array
+  array.collect{|x|x**2}
 end
