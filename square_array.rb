ff1b5203daf9aaf6298eed09961047fec8e0a8b3
Thomas Viles <thomas.c.viles.jr@gmail.com>
2019-06-09 20:05:09 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,7 @@
 def square_array(array)
   new_array = []
   array.each do |number|
-    new_number = number * number
-    new_array.push (new_number)
+    new_array.push (number**2)
   end
   return new_array
 end
